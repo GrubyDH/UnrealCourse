584cc5e605d30dbeef127f1e8de2ddba3d2f8813
Pawel Kozlowski <pawel.kozlowski@outlook.com>
2018-03-10 03:19:30 +0100

    GetName()

--- BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReporter.h"
+#include "GameFramework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -19,7 +20,8 @@ void UPositionReporter::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("Position reporter reporting for duty on Chair!"));
+	FString objectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position reporter for %s!"), *objectName);
 	
 }
 
